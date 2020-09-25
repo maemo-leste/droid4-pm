2e59c53cf6eacc3019338d7bb3072c35b338234c
uvos <devnull@uvos.xyz>
2020-09-25 11:26:57 +0200

    Fix first touch on screen on being lost Remove modem idle hack

--- usr/share/droid4-powermanagement/display-state-pm.sh
+++ usr/share/droid4-powermanagement/display-state-pm.sh
@@ -3,10 +3,10 @@
 if [ $5 == "on" ]
 then
         echo 20000 >  /sys/bus/i2c/drivers/atmel_mxt_ts/1-004a/power/autosuspend_delay_ms
+        echo on > /sys/bus/i2c/drivers/atmel_mxt_ts/1-004a/power/control
+        echo auto > /sys/bus/i2c/drivers/atmel_mxt_ts/1-004a/power/control
         printf 'U1234AT+SCRN=1\r' > /dev/gsmtty1
 else
         echo 100 > /sys/bus/i2c/drivers/atmel_mxt_ts/1-004a/power/autosuspend_delay_ms
         printf 'U1234AT+SCRN=0\r' > /dev/gsmtty1
-        # hack to workaround idle bug
-        head -c 0 /dev/ttyUSB{3,4}
 fi
